ce977c40bfc60dd0f53dccdbbbd0da3863f654d5
Luke <luke.milsom1@hotmail.com>
2020-09-26 21:06:23 +1000

    finalised question 2 for putin character, finishing up for the night, continuing putin tomorrow

--- src/Are_You_Smarter_Than_A_World_Leader.rb
+++ src/Are_You_Smarter_Than_A_World_Leader.rb
@@ -54,145 +54,220 @@ end
 #/////Characters////////////////////////////////////////////////////////////////////////////////////////////
 
 class Vladmir_Putin
-    def initialize
-      @score = []
-      @ethical_answers = ['Close the Borders']
-      @balanced_answers = ['Consult KGB']
-      @financial_answers = ['Leave The Borders Open']
-      @easter_egg = ['Travel to St Petersburg to address the citizens']
+  attr_accessor :q1
 
-    end
-# ------WELCOME MESSAGE--------------------------------
-  def message
-    puts "Congratulations Comrade, You have selected the Rootin' Tootin' Putin to battle this deadly virus, glory to the motherland"
-  end
-# ------BEGIN GAME SIMULATION---------------------------
-  def run_game
-    @prompt = TTY::Prompt.new
-    print "
-    ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    
-    ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    
-    ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      
-    ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      
-    ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    
-     ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    
-                                                                      
-    ████████╗ ██████╗                                                 
-    ╚══██╔══╝██╔═══██╗                                                
-       ██║   ██║   ██║                                                
-       ██║   ██║   ██║                                                
-       ██║   ╚██████╔╝                                                
-       ╚═╝    ╚═════╝                                                 
-                                                                      
-    ███╗   ███╗ ██████╗ ███████╗ ██████╗ ██████╗ ██╗    ██╗           
-    ████╗ ████║██╔═══██╗██╔════╝██╔════╝██╔═══██╗██║    ██║           
-    ██╔████╔██║██║   ██║███████╗██║     ██║   ██║██║ █╗ ██║           
-    ██║╚██╔╝██║██║   ██║╚════██║██║     ██║   ██║██║███╗██║           
-    ██║ ╚═╝ ██║╚██████╔╝███████║╚██████╗╚██████╔╝╚███╔███╔╝           
-    ╚═╝     ╚═╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝            
-                                                                      
-    "
-  # ------LOADING SPINNER-----------------------
-    spinner = TTY::Spinner.new("Loading :spinner ... ", format: :spin_2)
-    20.times do
-    spinner.spin
-    sleep(0.1)
-    end
-#--------SLOW TYPWRITE ANIMATION-------------------
-    def anim(string)
-        string.each_char do |n|
-        print n  
-        slt =rand(0.02..0.08)
-        sleep(slt)
-    end
-    print "\n"
-    end
+        def initialize
+          @score = []
+          @questions = ["Evening comrade, the virus is spreading at a rate of knotts, we need to take imediate action!", 
+        "What would you like to do?"]
+          @ethical_answers = ['Close the Borders', 'Crack a fresh bottle of grey goose and sing ra ra rasputin']
+          @balanced_answers = ['Consult the KGB', 'Adress the public in a serious manner, and let them know the danger of the virus']
+          @financial_answers = ['Leave The Borders Open', 'Play down the situation to keep a healthy economy']
+          @easter_egg = ['Travel to St Petersburg to address the citizens']
 
-    def sltime
-        time = rand(0.5..1.5)
-        sleep(time)
-    end
-# -------FAST TYPEWRITER ANIMATION-----------------
-    def anim_two(string)
-      string.each_char do |n|
-      print n  
-      slt =rand(0..0.001)
-      sleep(slt)
-  end
-  print "\n"
-  end
+        end
+    # ------WELCOME MESSAGE--------------------------------
+      def message
+        puts "Congratulations Comrade, You have selected the Rootin' Tootin' Putin to battle this deadly virus, glory to the motherland"
+      end
+    # ------BEGIN GAME SIMULATION---------------------------
+      def run_game
+        @prompt = TTY::Prompt.new
+        print "
+                      ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    
+                      ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    
+                      ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      
+                      ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      
+                      ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    
+                      ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    
+                                                                                        
+                     ████████╗ ██████╗                                                 
+                      ══██╔══╝██╔═══██╗                                                
+                        ██║   ██║   ██║                                                
+                        ██║   ██║   ██║                                                
+                        ██║   ╚██████╔╝                                                
+                        ╚═╝    ╚═════╝                                                 
+                                                                                        
+                      ███╗   ███╗ ██████╗ ███████╗ ██████╗ ██████╗ ██╗    ██╗           
+                      ████╗ ████║██╔═══██╗██╔════╝██╔════╝██╔═══██╗██║    ██║           
+                      ██╔████╔██║██║   ██║███████╗██║     ██║   ██║██║ █╗ ██║           
+                      ██║╚██╔╝██║██║   ██║╚════██║██║     ██║   ██║██║███╗██║           
+                      ██║ ╚═╝ ██║╚██████╔╝███████║╚██████╗╚██████╔╝╚███╔███╔╝           
+                      ╚═╝     ╚═╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝            
+                                                                                        
+        "
+      # ------LOADING SPINNER-----------------------
+      def spinner
+        spinner = TTY::Spinner.new("Loading :spinner ... ", format: :spin_2)
+        20.times do
+        spinner.spin
+        sleep(0.1)
+        end
+      end
+       spinner
+    #--------SLOW TYPWRITER ANIMATION-------------------
+        def anim(string)
+            string.each_char do |n|
+            print n  
+            slt =rand(0.02..0.08)
+            sleep(slt)
+        end
+        print "\n"
+        end
 
-  def sltime_two
-      time = rand(0.00..0.01)
-      sleep(time)
-  end
-
-#--------------------------------------------
-# ----QUESTION ONE---------------------------
-    anim("Evening comrade, the virus is spreading at a rate of knotts, we need to take imediate action!")
-    
-    q1 = [@ethical_answers[0], @balanced_answers[0], @financial_answers[0]]
-    `clear`
-    result_q1 = @prompt.collect do
-      key(:answer).select("Operation:", q1)
+        def sltime
+            time = rand(0.5..1.5)
+            sleep(time)
+        end
+    # -------FAST TYPEWRITER ANIMATION-----------------
+        def anim_two(string)
+          string.each_char do |n|
+          print n  
+          slt =rand(0..0.001)
+          sleep(slt)
+      end
+      print "\n"
       end
 
-    puts `clear`
-
-    result = result_q1.values.join
-   
-    # -----IF Q1 = KGB-----------------------
-  def kgb 
-       
-    anim_two("     
-                                ▄█   ▄█▄    ▄██████▄  ▀█████████▄  
-                                ███ ▄███▀   ███    ███   ███    ███ 
-                                ███▐██▀     ███    █▀    ███    ███ 
-                              ▄█████▀     ▄███         ▄███▄▄▄██▀  
-                              ▀▀█████▄    ▀▀███ ████▄  ▀▀███▀▀▀██▄  
-                                ███▐██▄     ███    ███   ███    ██▄ 
-                                ███ ▀███▄   ███    ███   ███    ███ 
-                                ███   ▀█▀   ████████▀  ▄█████████▀  
-                                ▀                                   
-                                
-                                ┬ ┬┌─┐┌─┐┌┬┐┌─┐ ┬ ┬┌─┐┬─┐┌┬┐┌─┐┬─┐┌─┐
-                                ├─┤├┤ ├─┤ │││─┼┐│ │├─┤├┬┘ │ ├┤ ├┬┘└─┐
-                                ┴ ┴└─┘┴ ┴─┴┘└─┘└└─┘┴ ┴┴└─ ┴ └─┘┴└─└─┘
-    ")
-      anim('    Донжт интерфере ин оур интернал аффаирс. Лет ус странгле оур цитиизенс ин пеацер.
-    ("Don\'t interfere in our internal affairs. We are playing Counter Strike this week.")')    
-    puts "\n"
-    puts "      The KGB has declined any involvement in the assistance of dealing with this virus"
-    puts "\n"
-    egg1 = [@easter_egg[0]]
-    result_egg1 = @prompt.collect do
-      key(:answer).select("Press Enter:", egg1)
+      def sltime_two
+          time = rand(0.00..0.01)
+          sleep(time)
       end
 
-    puts `clear`
-
-     egg_result = result_egg1.values.join
-
-    #  turn into a ternary
-          if egg_result = @easter_egg[0]
-            puts st_Petersburg
-            else puts "goodbye"
+    #--------------------------------------------
+    # ----QUESTION ENGINE----------Q1!!!----------------
+    def question_engine(q1, a1, a2, a3)
+        anim(q1)
+        
+        q1 = [a1, a2, a3]
+        `clear`
+        result_q1 = @prompt.collect do
+          key(:answer).select("Operation:", q1)
           end
-  end
-    #------------------------------------------------------------ 
-    if result == "Consult KGB"
-        puts kgb
-    else 
-        puts st_Petersburg
+
+        puts `clear`
+
+      result_q1.values.join
     end
- 
+    @q1 = question_engine(@questions[0], @ethical_answers[0], @balanced_answers[0], @financial_answers[0])
+      
+        # -----IF Q1 = KGB-----------------------
+      def kgb 
+          
+        anim_two("     
+                                    ▄█   ▄█▄    ▄██████▄  ▀█████████▄  
+                                    ███ ▄███▀   ███    ███   ███    ███ 
+                                    ███▐██▀     ███    █▀    ███    ███ 
+                                  ▄█████▀     ▄███         ▄███▄▄▄██▀  
+                                  ▀▀█████▄    ▀▀███ ████▄  ▀▀███▀▀▀██▄  
+                                    ███▐██▄     ███    ███   ███    ██▄ 
+                                    ███ ▀███▄   ███    ███   ███    ███ 
+                                    ███   ▀█▀   ████████▀  ▄█████████▀  
+                                    ▀                                   
+                                    
+                                    ┬ ┬┌─┐┌─┐┌┬┐┌─┐ ┬ ┬┌─┐┬─┐┌┬┐┌─┐┬─┐┌─┐
+                                    ├─┤├┤ ├─┤ │││─┼┐│ │├─┤├┬┘ │ ├┤ ├┬┘└─┐
+                                    ┴ ┴└─┘┴ ┴─┴┘└─┘└└─┘┴ ┴┴└─ ┴ └─┘┴└─└─┘
+        ")
+          anim('    Донжт интерфере ин оур интернал аффаирс. Лет ус странгле оур цитиизенс ин пеацер.
+        ("Don\'t interfere in our internal affairs. We are playing Counter Strike this week.")')    
+        puts "\n"
+        puts "      The KGB has declined any involvement in the assistance of dealing with this virus"
+        puts "\n"
+        egg1 = [@easter_egg[0]]
+        result_egg1 = @prompt.collect do
+          key(:answer).select("Press Enter:", egg1)
+          end
+
+        puts `clear`
+
+        egg_result = result_egg1.values.join
+
+        #  turn into a ternary
+        
+              if egg_result = @easter_egg[0]
+                puts st_Petersburg
+                else puts "goodbye"
+              end
+      end
+  
+    #---------QUESTION TWO--------------------------------------------------- 
+def st_Petersburg
+  
+    anim_two( "
+                      ██████████████████████████████████████████████████████████████████████████▀█
+                      █─▄▄▄▄█─▄─▄─███▄─▄▄─█▄─▄▄─█─▄─▄─█▄─▄▄─█▄─▄▄▀█─▄▄▄▄█▄─▄─▀█▄─██─▄█▄─▄▄▀█─▄▄▄▄█
+                      █▄▄▄▄─███─██████─▄▄▄██─▄█▀███─████─▄█▀██─▄─▄█▄▄▄▄─██─▄─▀██─██─███─▄─▄█─██▄─█
+                      ▀▄▄▄▄▄▀▀▄▄▄▀▀▀▀▄▄▄▀▀▀▄▄▄▄▄▀▀▄▄▄▀▀▄▄▄▄▄▀▄▄▀▄▄▀▄▄▄▄▄▀▄▄▄▄▀▀▀▄▄▄▄▀▀▄▄▀▄▄▀▄▄▄▄▄▀
+                      
+                      ")
+                      def spinner
+                        spinner = TTY::Spinner.new("Loading :spinner ... ", format: :spin_2)
+                        20.times do
+                        spinner.spin
+                        sleep(0.1)
+                        end
+                      end
+                       spinner
+
+    def question_engine(q1, a1, a2, a3)
+      anim("\nThank god you're here sir, did the flight from Moscow treat you well? \n 
+      The pressure is mounting our citizens are begining to feel unwell,\n
+      the public is growing weary that the virus is growing out of control due to your decision to #{@q1.downcase}\n
+      you need to adress them:")
+      puts q1
+      
+      q1 = [a1, a2, a3]
+      `clear`
+      result_q1 = @prompt.collect do
+        key(:answer).select("", q1)
+    end
+
+      puts `clear`
+
+    answer = result_q1.values.join
+    end
+    @q2 = question_engine(@questions[1], @ethical_answers[1], @balanced_answers[1], @financial_answers[1]) 
+  
+end
+# -----------RARA rasputin-----------------------
+def ra_ra
+puts `clear`
+puts "
+⊂_ヽ
+　 ＼＼
+　　 ＼( ͡° ͜ʖ ͡°) i dance
+　　　 >　⌒ヽ
+　　　/ 　 へ＼
+　　 /　　/　＼＼
+　　 ﾚ　ノ　　 ヽ_つ
+　　/　/
+　 /　/|
+　(　(ヽ
+　|　|、＼
+　| 丿 ＼ ⌒)
+　| |　　) /
+ノ )　　Lﾉ
+(_／
+"
+anim('Шчатс готтен инто ыоу ВЛадмир..? ("What\'s gotten into you Vladmir...?")')
+end
+    # ---Putin Execution Logic -----
+    if @q1 == "Consult KGB"
+        puts kgb 
+    else
+      puts st_Petersburg
+    end
+   
+    if @q2 == 'Crack a fresh bottle of grey goose and sing ra ra rasputin'
+      puts ra_ra
+   else 
+    puts "wassssssup"
+   end
+
+
+
     
-
-
-
-
-
-
 
 
 
@@ -248,15 +323,24 @@ puts "1 For #{characters[0]}"
 puts "2 for #{characters[1]}"
 puts "3 for #{characters[2]}"
 
-selection = gets.to_i
 
-if selection == 1
-    selection = donald
-elsif selection == 2
-    selection = vlad
-else 
-    selection = boris
-end 
+while true
+    selection = gets.to_i
+  if selection == 1
+      selection = donald
+      break
+  elsif selection == 2
+      selection = vlad
+      break
+  elsif
+      selection == 3
+      selection = boris
+      break
+  else 
+    puts "Incorrect input, please enter a number from 1-3"
+  end 
+end
+
 
   puts divider
   puts selection.message
@@ -266,12 +350,6 @@ end
   puts selection.run_game
 
 
-
-
-
-
-# choices = %w(vodka beer wine whisky bourbon)
-# prompt.multi_select("Select drinks?", choices)
 
 
 
