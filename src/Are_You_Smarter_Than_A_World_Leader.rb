965b2d3e2a6dcfcaa7adf353ba0558eb0f8eeb88
Luke <luke.milsom1@hotmail.com>
2020-09-26 14:24:54 +1000

    Working on Vladmir Putin character game path, added a file named test.rb to test functions

--- src/Are_You_Smarter_Than_A_World_Leader.rb
+++ src/Are_You_Smarter_Than_A_World_Leader.rb
@@ -1,31 +1,29 @@
 require 'tty-progressbar'
+require 'tty-spinner'
+require 'pastel'
+require "tty-prompt"
+
 
 divider = "################################################################################################################"
 
-class Character
-    def initialize
-
-    end
-
-end
 
 class Game
-     
+  attr_accessor :intro
     def initialize
     
     @score = []
     @divider = "################################################################################################################"
     @countries = ["China", "Bosnia & Herzegovina", "Jamaica" , "Canada", "Kyrgyztan", "Cuba", "Belarus", "The Philippines"]
-    @questions = []
+    @questions = [""]
     @country = @countries.sample
     end
-
     def dialogue 
-        puts "The year is 2019 - an unknown virus has broken out in #{@country}, and it is spreading across the globe, 
+      
+        @intro = "The year is 2019 - an unknown virus has broken out in #{@country}, and it is spreading across the globe, 
         it is your job as supreme leader to lead your people and your country through this.\nBe careful how you answer, as these actions could lead to adverse politcal and ethical results!!"
-    end
+    
+      end
     def continue 
-  
         while true
         press_enter = gets.chomp
         if press_enter.empty?
@@ -35,7 +33,7 @@ class Game
         end
         end
     end
-    def run
+    def engine
         puts @divider
         puts dialogue
         puts @divider
@@ -45,7 +43,7 @@ class Game
         100.times do
         sleep(0.02)
         bar.advance(1)
-end
+        end
 
 
 
@@ -57,6 +55,7 @@ end
 
 class Vladmir_Putin
     def initialize
+      @score = []
       @ethical_answers = []
       @balanced_answers = []
       @financial_answers = []
@@ -65,6 +64,74 @@ class Vladmir_Putin
 
   def message
     puts "Congratulations Comrade, You have selected the Rootin' Tootin' Putin to battle this deadly virus, glory to the motherland"
+  end
+
+  def run_game
+    @prompt = TTY::Prompt.new
+    print "
+    ██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗    
+    ██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝    
+    ██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗      
+    ██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝      
+    ╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗    
+     ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝    
+                                                                      
+    ████████╗ ██████╗                                                 
+    ╚══██╔══╝██╔═══██╗                                                
+       ██║   ██║   ██║                                                
+       ██║   ██║   ██║                                                
+       ██║   ╚██████╔╝                                                
+       ╚═╝    ╚═════╝                                                 
+                                                                      
+    ███╗   ███╗ ██████╗ ███████╗ ██████╗ ██████╗ ██╗    ██╗           
+    ████╗ ████║██╔═══██╗██╔════╝██╔════╝██╔═══██╗██║    ██║           
+    ██╔████╔██║██║   ██║███████╗██║     ██║   ██║██║ █╗ ██║           
+    ██║╚██╔╝██║██║   ██║╚════██║██║     ██║   ██║██║███╗██║           
+    ██║ ╚═╝ ██║╚██████╔╝███████║╚██████╗╚██████╔╝╚███╔███╔╝           
+    ╚═╝     ╚═╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝            
+                                                                      
+    "
+    spinner = TTY::Spinner.new("Loading :spinner ... ", format: :spin_2)
+    20.times do
+    spinner.spin
+    sleep(0.1)
+    end
+
+    def anim(string)
+        string.each_char do |n|
+        print n  
+        slt =rand(0.02..0.08)
+        sleep(slt)
+    end
+    print "\n"
+    end
+
+    def sltime
+        time = rand(0.5..1.5)
+        sleep(time)
+    end
+
+    
+
+    anim("Evening comrade, the virus is spreading at a rate of knotts, we need to take imediate action!")
+    
+    q1 = %w[Close_the_Borders Consult_KGB Leave_The_Borders_Open]
+    `clear`
+    result_q1 = @prompt.collect do
+      key(:env).select("Operation:", q1)
+      end
+
+    
+
+
+
+
+
+
+
+
+
+
   end
   
 end
@@ -79,6 +146,9 @@ class Donald_Trump
   def message
     puts "You have selected the one the only Donald Trump to battle this deadly virus, god bless America!"
   end
+  def debrief
+    puts ""
+  end
   
 end
 
@@ -92,13 +162,18 @@ class Boris_Johnson
   def message
     puts "Rigtho Chap, Boris Johnson here looks like Brexit came early!"
   end
+  def debrief
+    puts ""
+  end
   
-end
-    donald = Donald_Trump.new
-    vlad = Vladmir_Putin.new
-    boris = Boris_Johnson.new
+  end
+  
 
 #////////////////////////////////////////////////////////////////////////////////////////////////////////////
+donald = Donald_Trump.new
+vlad = Vladmir_Putin.new
+boris = Boris_Johnson.new
+
 characters = ["Donald Trump", "Vladmir Putin", "Boris Johnson"]
 puts "\n"
 puts "A pandemic has broken out!!"
@@ -118,14 +193,12 @@ else
     selection = boris
 end 
 
-
-#/////////RUN///////////////////////////////////////////////////////////////////////////////////////////////
-
   puts divider
   puts selection.message
   game_one = Game.new
-  puts game_one.run
-  puts selection
+  puts game_one.engine
+  puts `clear`
+  puts selection.run_game
 
 
 
---
+++ src/test.rb
@@ -0,0 +1,32 @@
+require "json"
+require "tty-prompt"
+
+prompt = TTY::Prompt.new
+envs = %w[local development test staging production]
+platforms = %w[Debian Ubuntu Fedora Windows macOS]
+
+puts TTY::Cursor.save
+
+result = prompt.collect do
+  key(:username).ask("Username:")
+  key(:password).mask("Password:")
+  key(:env).select("Environment:", envs)
+  key(:version).ask("Version (1-10)?", convert: :int, in: (1..10))
+  key(:verbose).yes?("Verbose?")
+  key(:platforms).multi_select("Platforms?", platforms)
+  key(:nodes).slider("Number of nodes?", max: 20, step: 1)
+end
+
+print TTY::Cursor.clear_screen_up
+print TTY::Cursor.restore + TTY::Cursor.show
+puts
+puts JSON.pretty_generate(result)
+puts
+
+
+
+envs = %w[local development test staging production]
+ 
+result = prompt.collect do
+key(:env).select("Environment:", envs)
+end
