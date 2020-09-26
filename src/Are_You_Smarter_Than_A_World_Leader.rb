443493f45e4146654fb18fa49af720a9685692ed
Luke <luke.milsom1@hotmail.com>
2020-09-26 17:45:25 +1000

    Finished KGB Putin nested easter egg

--- src/Are_You_Smarter_Than_A_World_Leader.rb
+++ src/Are_You_Smarter_Than_A_World_Leader.rb
@@ -56,16 +56,17 @@ end
 class Vladmir_Putin
     def initialize
       @score = []
-      @ethical_answers = []
-      @balanced_answers = []
-      @financial_answers = []
+      @ethical_answers = ['Close the Borders']
+      @balanced_answers = ['Consult KGB']
+      @financial_answers = ['Leave The Borders Open']
+      @easter_egg = ['Travel to St Petersburg to address the citizens']
 
     end
-
+# ------WELCOME MESSAGE--------------------------------
   def message
     puts "Congratulations Comrade, You have selected the Rootin' Tootin' Putin to battle this deadly virus, glory to the motherland"
   end
-
+# ------BEGIN GAME SIMULATION---------------------------
   def run_game
     @prompt = TTY::Prompt.new
     print "
@@ -91,12 +92,13 @@ class Vladmir_Putin
     ╚═╝     ╚═╝ ╚═════╝ ╚══════╝ ╚═════╝ ╚═════╝  ╚══╝╚══╝            
                                                                       
     "
+  # ------LOADING SPINNER-----------------------
     spinner = TTY::Spinner.new("Loading :spinner ... ", format: :spin_2)
     20.times do
     spinner.spin
     sleep(0.1)
     end
-
+#--------SLOW TYPWRITE ANIMATION-------------------
     def anim(string)
         string.each_char do |n|
         print n  
@@ -110,17 +112,80 @@ class Vladmir_Putin
         time = rand(0.5..1.5)
         sleep(time)
     end
+# -------FAST TYPEWRITER ANIMATION-----------------
+    def anim_two(string)
+      string.each_char do |n|
+      print n  
+      slt =rand(0..0.001)
+      sleep(slt)
+  end
+  print "\n"
+  end
 
-    
+  def sltime_two
+      time = rand(0.00..0.01)
+      sleep(time)
+  end
 
+#--------------------------------------------
+# ----QUESTION ONE---------------------------
     anim("Evening comrade, the virus is spreading at a rate of knotts, we need to take imediate action!")
     
-    q1 = %w[Close_the_Borders Consult_KGB Leave_The_Borders_Open]
+    q1 = [@ethical_answers[0], @balanced_answers[0], @financial_answers[0]]
     `clear`
     result_q1 = @prompt.collect do
-      key(:env).select("Operation:", q1)
+      key(:answer).select("Operation:", q1)
       end
 
+    puts `clear`
+
+    result = result_q1.values.join
+   
+    # -----IF Q1 = KGB-----------------------
+  def kgb 
+       
+    anim_two("     
+                                ▄█   ▄█▄    ▄██████▄  ▀█████████▄  
+                                ███ ▄███▀   ███    ███   ███    ███ 
+                                ███▐██▀     ███    █▀    ███    ███ 
+                              ▄█████▀     ▄███         ▄███▄▄▄██▀  
+                              ▀▀█████▄    ▀▀███ ████▄  ▀▀███▀▀▀██▄  
+                                ███▐██▄     ███    ███   ███    ██▄ 
+                                ███ ▀███▄   ███    ███   ███    ███ 
+                                ███   ▀█▀   ████████▀  ▄█████████▀  
+                                ▀                                   
+                                
+                                ┬ ┬┌─┐┌─┐┌┬┐┌─┐ ┬ ┬┌─┐┬─┐┌┬┐┌─┐┬─┐┌─┐
+                                ├─┤├┤ ├─┤ │││─┼┐│ │├─┤├┬┘ │ ├┤ ├┬┘└─┐
+                                ┴ ┴└─┘┴ ┴─┴┘└─┘└└─┘┴ ┴┴└─ ┴ └─┘┴└─└─┘
+    ")
+      anim('    Донжт интерфере ин оур интернал аффаирс. Лет ус странгле оур цитиизенс ин пеацер.
+    ("Don\'t interfere in our internal affairs. We are playing Counter Strike this week.")')    
+    puts "\n"
+    puts "      The KGB has declined any involvement in the assistance of dealing with this virus"
+    puts "\n"
+    egg1 = [@easter_egg[0]]
+    result_egg1 = @prompt.collect do
+      key(:answer).select("Press Enter:", egg1)
+      end
+
+    puts `clear`
+
+     egg_result = result_egg1.values.join
+
+    #  turn into a ternary
+          if egg_result = @easter_egg[0]
+            puts st_Petersburg
+            else puts "goodbye"
+          end
+  end
+    #------------------------------------------------------------ 
+    if result == "Consult KGB"
+        puts kgb
+    else 
+        puts st_Petersburg
+    end
+ 
     
 
 
